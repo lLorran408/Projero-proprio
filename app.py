38ae5c6bb5107f0b759db57e3971cb5cda9b1cc8
lLorran408 <elorran321@gmail.com>
2025-11-26 12:47:37 -0300

    fix

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 from flask import Flask, render_template
 
+# Cria uma instância da aplicação Flask
 app = Flask(__name__)
 
-app = Flask(__name__)
-
+# Define uma rota (o endereço que o usuário irá acessar)
+# Neste caso, a raiz da aplicação ("/")
 @app.route('/')
-def hello_world():
-    return '<h1></h1>'
+def home():
+    # Usa a função render_template para buscar e renderizar
+    # o arquivo 'index.html' que está na pasta 'templates'
+    return render_template ('pagina3.html')
 
-@app.route('/index')
-def index():
-    return 'Index Page'
+@app.route('/pagina2.html')
+def pagina2_page():
+    return render_template("/pagina2.html")
 
-@app.route('/login')
-def login():
+@app.route('/index.html')
+def pagina3_page():
+    return render_template("/index.html")
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+@app.route('/pagina3.html')
+def pagina4_page():
+    return render_template("/pagina3.html")
 
+# Bloco para rodar a aplicação
 if __name__ == '__main__':
+    # Roda a aplicação no modo debug, o que ajuda no desenvolvimento
     app.run(debug=True)
---
+++ static/js/firebase_config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjJ9HAh1J3CEsDg7PfRZSsJr0p0qaqGM0",
+  authDomain: "projeto-banco-de-dados-f9f28.firebaseapp.com",
+  projectId: "projeto-banco-de-dados-f9f28",
+  storageBucket: "projeto-banco-de-dados-f9f28.firebasestorage.app",
+  messagingSenderId: "492974814052",
+  appId: "1:492974814052:web:0322a973f1ef4602fe8071",
+  measurementId: "G-5XXS6YW6CH"
+};
